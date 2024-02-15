92504da0f30da924cf98a29acec4b10b6cd96bdd
fuck <asdgfsg235@gmail.com>
2024-02-15 18:30:42 +0300

    producer refactored a bit

---
+++ internal/util/orderPlacer.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type (
+	OrderPlacer struct {
+		producer   *kafka.Producer
+		topic      string
+		deliveryCh chan kafka.Event
+	}
+)
+
+func NewOrderPlacer(p *kafka.Producer, topic string) *OrderPlacer {
+
+	return &OrderPlacer{
+		producer:   p,
+		topic:      topic,
+		deliveryCh: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderPlacer) PlaceOrder(orderType string, size int) error {
+
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &op.topic,
+			Partition: kafka.PartitionAny},
+		Value: payload,
+	},
+		op.deliveryCh)
+	if err != nil {
+		fmt.Printf("failed to produce message: %v\n", err)
+	} else {
+		fmt.Printf("Produced message: %s\n", payload)
+	}
+
+	<-op.deliveryCh
+	return nil
+}
--- producer/producer.go
+++ producer/producer.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
+	"github.com/Karanth1r3/kafka_learn/internal/util"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+const (
+	globalTopic = "coordinates"
+)
+
 func main() {
+
 	// Producer config
 	config := &kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
@@ -23,25 +28,39 @@ func main() {
 	}
 	defer kp.Close()
 
-	// Creating topic
-	topic := "coordinates"
-	deliverCh := make(chan kafka.Event, 10000)
+	// Wrapping producer into the app conext-based structure
+	op := util.NewOrderPlacer(kp, globalTopic)
 
-	// Writing message to the topic
-	for i := 0; i < 10; i++ {
-		value := fmt.Sprintf("message %d", i)
-		err := kp.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic,
-				Partition: kafka.PartitionAny},
-			Value: ([]byte(value)),
-		}, deliverCh)
-		if err != nil {
-			fmt.Printf("failed to produce message %d: %v\n", i, err)
-		} else {
-			fmt.Printf("Produced message: %d: %s\n", i, value)
+	// Send 1000 messages
+	for i := 0; i < 1000; i++ {
+		if err := op.PlaceOrder("market", i); err != nil {
+			log.Fatal(err)
 		}
-		<-deliverCh
 		time.Sleep(time.Second * 4)
 	}
-	//kp.Flush(15 * 1000)
+
+	// TODO - get rid of it when learning is over
+	/*
+		// Creating topic
+		topic := "coordinates"
+		deliverCh := make(chan kafka.Event, 10000)
+
+		// Writing message to the topic
+		for i := 0; i < 10; i++ {
+			value := fmt.Sprintf("message %d", i)
+			err := kp.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic,
+					Partition: kafka.PartitionAny},
+				Value: ([]byte(value)),
+			}, deliverCh)
+			if err != nil {
+				fmt.Printf("failed to produce message %d: %v\n", i, err)
+			} else {
+				fmt.Printf("Produced message: %d: %s\n", i, value)
+			}
+			<-deliverCh
+			time.Sleep(time.Second * 4)
+		}
+		//kp.Flush(15 * 1000)
+	*/
 }
